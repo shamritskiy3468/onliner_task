42ca7187ec74cfbdee923d2216688dd2b3f6a968
shamritskiy3468 <shamritskiy3468@gmail.com>
2018-11-24 16:56:38 +0300

    Attemps to decompose classes and methods

--- csv_writer.rb
+++ csv_writer.rb
@@ -4,6 +4,17 @@ class CSVWriter
 		@file = file
 	end
 
+	def prepare_data_to_writing(a,b,c)
+		data_to_write = []
+		(0..a.size).each do |i|
+			temp = []
+			temp << a[i]
+			temp << b[i]
+			temp << c[i]
+			data_to_write << temp
+		end
+	end	
+
 	def write_data(data_to_write)
 		CSV.open(@file, 'a+') do |csv|
 			(0..data_to_write.size).each do |index|
--- scrapper.rb
+++ scrapper.rb
@@ -8,23 +8,33 @@ class Scrapper
 	def initialize
 		@options = Selenium::WebDriver::Chrome::Options.new(args: ['headless']) 
 		@driver = Selenium::WebDriver.for(:chrome, options: @options).get('https://www.onliner.by/')
+		@images, @titles, @contens = [], [], []
 	end
 
 	def run_onliner_scrapper
 		go_to_news
+		2.times { get_all_info }
+	end
+
+	def get_all_info
+		find_images_hrefs
+		find_news_content
+		find_news_titles
+
+		writer = CSVWriter.new('onliner_data.csv')
+		writer.write_data		
 	end
 
 	def find_images_hrefs
-		news_images = NewsImages.new
-		news_images = 
+		@images = scrap_imges
 	end
 
 	def find_news_content
-
+		@images = scrap_contents
 	end
 
 	def find_news_titles
-
+		@titles = scrap_titles
 	end	
 
 private
@@ -37,7 +47,7 @@ private
 		elements_array.uniq!
 	end
 
-	def scrap_headers
+	def scrap_titles
 		elements_array = []
 		@driver.find_elements(:class, TITLE).each do |item|
 			elements_array << item.text[0..199] if item.text.to_s.size != 0
@@ -53,7 +63,7 @@ private
 		elements_array.uniq!
 	end
 
-	def get_more_news
+	def get_more_news	
 		@driver.find_element(:class, 'news-more__button').click
 	end
 
